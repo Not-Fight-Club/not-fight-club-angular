4f8416b434004cfbccbc42c84f459f4c1ebf5d39
KyleHillRevature <kyle.hill@revature.net>
2021-10-15 03:26:14 -0400

    added blank urls in env.prod so that will build

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  bets: 'http://20.81.21.138'
+  bets: 'http://20.81.21.138',
+  fightsApiUrl: "http://",
+  charactersApiUrl: "http://",
+  productsApiUrl: "http://",
+  usersApiUrl: "http://"
 };
