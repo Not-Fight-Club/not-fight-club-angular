63d2778b77a8293863863b74193951e2d5b3a63c
zachary woolfolk <zachary.woolfolk@revature.net>
2021-10-19 13:59:31 -0500

    added UI feedback after voting

--- Angular/src/app/vote/vote.component.ts
+++ Angular/src/app/vote/vote.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Vote } from '../interfaces/vote';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { FightService } from '../service/fight/fight.service';
 import { Fighter } from '../interfaces/fighter';
 import { Fight } from '../interfaces/fight';
@@ -12,17 +12,20 @@ import { Fight } from '../interfaces/fight';
 })
 export class VoteComponent implements OnInit {
 
+  notVoted: boolean = true;
+
+
   @Input() fighter1: Fighter = {
-    fighterId: 2,
-    fightId: 2,
-    characterId: 1,
+    fighterId: 0,
+    fightId: 0,
+    characterId: 0,
     votes: 0,
     isWinner: false
   };
   @Input() fighter2: Fighter = {
-    fighterId: 6,
-    fightId: 2,
-    characterId: 2,
+    fighterId: 0,
+    fightId: 0,
+    characterId: 0,
     votes: 0,
     isWinner: false
   };
@@ -33,9 +36,10 @@ export class VoteComponent implements OnInit {
   constructor(private http: HttpClient, private fightService: FightService) { }
 
   ngOnInit(): void {
-    this.tallyVotes(this.fighter1.fightId, this.fighter1.fighterId);
-    this.tallyVotes(this.fighter2.fightId, this.fighter2.fighterId);
+    //this.tallyVotes(this.fighter1.fightId, this.fighter1.fighterId);
+    //this.tallyVotes(this.fighter2.fightId, this.fighter2.fighterId);
 
+    this.checkUserVote(this.fighter1.fightId, this.getUserId())
   }
 
   submitVote(fighter: Fighter) {
@@ -48,8 +52,27 @@ export class VoteComponent implements OnInit {
     let userStr = sessionStorage.getItem("user");
     if (!userStr) { return }
     vote.userId = JSON.parse(userStr).userId;
+    this.notVoted = false;
     console.log(`You, User ${vote.userId} have voted for FIGHTER _${vote.fighterId} in fight ${vote.fightId}`);
     this.fightService.castVote(vote).subscribe();
+  }
+
+  getUserId() {
+    let userStr = sessionStorage.getItem("user");
+    if (!userStr) { return }
+    return JSON.parse(userStr).userId;
+  }
+
+  checkUserVote(fightId: number, userId: number) {
+    return this.fightService.checkUserVote(fightId, userId).subscribe((voted) => {
+      console.log(voted);
+      if (voted) {
+        this.notVoted = false;
+      }
+      else {
+        this.notVoted = true;
+      }
+    })
   }
 
   tallyVotes(fightId: number, fighterId: number) {
